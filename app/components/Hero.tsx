f365e87db9ce30e29fca75a6c273c286fd209397
DhyeyKansagara <DHYEYKANSAGARA@GMAIL.COM>
2023-11-12 03:32:07 -0500

    landing page completed

---
+++ app/components/Hero.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Image from "next/image";
+import {client, urlFor} from "../lib/sanity";
+
+async function getData(){
+    const query = "*[_type == 'heroImage'][0]";
+    const data = await client.fetch(query);
+    return data;
+}
+
+export default async function Hero() {
+  const data = await getData()  
+  return (
+    <section className="mx-auto max-w-2xl px-4 sm:pb-6 lg:max-w-7xl lg:px-8">
+        <div className="mb8 flex flex-wrap justify-between md:mb-16"> 
+            <div className="mb-6 flex w-full flex-col justify-center sm:mb-12 lg:mb-0 lg:w-1/3 lg:pb-24 lg:pt-48">
+                <h1 className="mb-4 text-4xl font-bold text-black sm:text-5xl md:mb-8 md:text-6xl">
+                    Wear your game!
+                </h1>
+                <p className="max-w-md leading-relaxed text-gray-500 xl:text-lg"> 
+                    Join the ranks of NJIT&apos;s most passionate soccer supporters. Explore exclusive shopNSC merchandise and fuel the spirit of our team!
+                </p>
+            </div>
+
+            <div className="mb-12 flex w-full md:mb-16 lg:w-2/3">
+                <div className="relative left-12 top-12 z-10 -ml-12 overflow-hidden rounded-lg bg-gray-100 shadow-lg md:left-16 md:top-16 lg:ml-0">
+                    <Image src={urlFor(data.image1).url()} alt="man in NSC merch" className="h-full w-full object-cover object-center" priority width={500} height={500}/>
+                </div>
+                <div className="overflow-hidden rounded-lg bg-gray-100 shadow-lg">
+                    <Image src={urlFor(data.image2).url()} alt="woman in NSC merch" className="h-full w-full object-cover object-center" priority width={500} height={500}/>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import Link from "next/link"
 import {usePathname} from "next/navigation";
 import { Button } from '@/components/ui/button';
+import { ShoppingBag } from 'lucide-react';
 
 const links = [
     {name: 'Home', href: '/'},
@@ -18,7 +19,7 @@ export default function Navbar() {
         <header className="mb-8 border-b">
             <div className="flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl">
                 <Link href="/">
-                    <h1 className="text-4xl font-bold">
+                    <h1 className="text-2xl md:text-4xl font-bold">
                         Shop<span className="text-primary">NSC</span>
                     </h1>
                 </Link>
@@ -40,7 +41,14 @@ export default function Navbar() {
                 </nav>
 
                 <div className="flex divide-x border-r sm:border-l">
-                        <Button> Hey </Button>
+                        <Button
+                        variant = {"outline"}
+                        className="flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 sm:w-20 md:h-24 md:w-24 rounded-none"> 
+                                <ShoppingBag/>
+                                <span className="hidden text-xs font-semibold text-gray-500 sm:block">
+                                    cart
+                                </span>
+                        </Button>
                 </div>
             </div>
         </header>
