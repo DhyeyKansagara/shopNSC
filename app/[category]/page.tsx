062498f4fa20d6076f5a6eaf1581b692da5c8297
DhyeyKansagara <DHYEYKANSAGARA@GMAIL.COM>
2023-11-23 18:35:29 -0500

    added shopping cart sheet

--- app/[category]/page.tsx
+++ app/[category]/page.tsx
@@ -25,7 +25,7 @@ export default async function CategoryPage({params}: {params: {category: string}
   
     return (
     <div className='bg-white'>
-            <div className='mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8'>
+            <div className='mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8'>
                 <div className='flex justify-between items-center'>
                     <h2 className='text-2xl font-bold tracking-tight text-gray-900'>
                         Products for {params.category}
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,6 +5,7 @@ import Link from "next/link"
 import {usePathname} from "next/navigation";
 import { Button } from '@/components/ui/button';
 import { ShoppingBag } from 'lucide-react';
+import { useShoppingCart } from 'use-shopping-cart';
 
 const links = [
     {name: 'Home', href: '/'},
@@ -14,6 +15,7 @@ const links = [
 ];
 
 export default function Navbar() {
+    const {handleCartClick} = useShoppingCart()
   const pathname = usePathname();
   return (
         <header className="mb-8 border-b">
@@ -43,6 +45,7 @@ export default function Navbar() {
                 <div className="flex divide-x border-r sm:border-l">
                         <Button
                         variant = {"outline"}
+                        onClick={() => handleCartClick()}
                         className="flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 sm:w-20 md:h-24 md:w-24 rounded-none"> 
                                 <ShoppingBag/>
                                 <span className="hidden text-xs font-semibold text-gray-500 sm:block">
